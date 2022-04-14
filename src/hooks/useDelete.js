46167893d8050804a679abd8f6ac98199d985bc6
dorian <dorianlegan@protonmail.com>
2022-04-14 11:25:35 +0100

    added error handler in delete

--- src/hooks/useDelete.js
+++ src/hooks/useDelete.js
@@ -1,11 +1,18 @@
+import { useNavigate } from "react-router-dom";
+
 const useDelete = () => {
+  const nav = useNavigate();
   const handleDelete = async (url) => {
-    await fetch(url, {
-      method: "DELETE",
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
+    try {
+      await fetch(url, {
+        method: "DELETE",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+    } catch (e) {
+      nav("/oops", { replace: true });
+    }
   };
   return [handleDelete];
 };
